fa9df3fc0c7c8a8d549a926af8a0af21412508f8
Christoph1812 <99895259+Christoph1812@users.noreply.github.com>
2023-09-08 16:39:21 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { MenuOverlayComponent } from './menu-overlay/menu-overlay.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    MyWorkComponent,
+    ContactComponent,
+    FooterComponent,
+    ImprintComponent,
+    PrivacyPolicyComponent,
+    MenuOverlayComponent
   ],
   imports: [
     BrowserModule,
