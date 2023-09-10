7dfc24af066bb7064d6fced27ef250b314e58535
Christoph1812 <99895259+Christoph1812@users.noreply.github.com>
2023-09-10 14:01:30 +0200

    added menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MyWorkComponent } from './my-work/my-work.component';
 import { ContactComponent } from './contact/contact.component';
@@ -12,19 +11,24 @@ import { FooterComponent } from './footer/footer.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { MenuOverlayComponent } from './menu-overlay/menu-overlay.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeroComponent } from './hero/hero.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HeaderComponent,
     AboutMeComponent,
     MyWorkComponent,
     ContactComponent,
     FooterComponent,
     ImprintComponent,
     PrivacyPolicyComponent,
-    MenuOverlayComponent
+    MenuOverlayComponent,
+    MainPageComponent,
+    HeroComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
