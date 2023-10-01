fe8bc3e8f77d8099ca484cfd64fa91b578936713
Christoph1812 <99895259+Christoph1812@users.noreply.github.com>
2023-10-01 18:12:05 +0200

    update footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
+  { path: 'imprint', component: ImprintComponent },
 ];
 
 @NgModule({
